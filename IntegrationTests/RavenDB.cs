561ff955c94d00b47045720efea7c6b13f458c53
Rob Kayman <rkayman@gmail.com>
2014-06-11 02:36:57 -0700

    Redo commits; update .net and nuget packages
    Change to .NET 4.5.1 which requires updating Nuget packages; add
    RavenDb
    package for new support

---
+++ IntegrationTests/RavenDB.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Embedded;
+using Raven.Client.Listeners;
+using SnowMaker;
+using SnowMaker.RavenDb;
+using System;
+
+namespace IntegrationTests.cs
+{
+	[TestFixture]
+	public class RavenDb : Scenarios<RavenDb.TestScope>
+	{
+		protected override IOptimisticDataStore BuildStore( TestScope scope )
+		{
+			return new RavenOptimisticDataStore( scope.Repository );
+		}
+
+		protected override TestScope BuildTestScope()
+		{
+			return new TestScope();
+		}
+
+		public class TestScope : ITestScope
+		{
+			public class NoStaleQueriesListener : IDocumentQueryListener
+			{
+				public void BeforeQueryExecuted( IDocumentQueryCustomization queryCustomization )
+				{
+					queryCustomization.WaitForNonStaleResults();
+				}
+			}
+
+			public TestScope()
+			{
+				var ticks = DateTime.UtcNow.Ticks;
+				IdScopeName = string.Format( "snowmakertest{0}", ticks );
+
+				var repository = new EmbeddableDocumentStore { RunInMemory = true };
+
+				repository.Initialize();
+				repository.Conventions.DefaultQueryingConsistency = ConsistencyOptions.AlwaysWaitForNonStaleResultsAsOfLastWrite;
+				repository.RegisterListener( new NoStaleQueriesListener() );
+
+				Repository = repository;
+			}
+
+			public string IdScopeName { get; private set; }
+
+			public IDocumentStore Repository { get; private set; }
+
+			public string ReadCurrentPersistedValue()
+			{
+				using (var session = Repository.OpenSession())
+				{
+					var state = session.Load<RavenStateDocument>( IdScopeName );
+					return state.Value;
+				}
+			}
+
+			public void Dispose()
+			{
+				if (Repository.WasDisposed)
+					return;
+
+				Repository.Dispose();
+			}
+		}
+	}
+}
+

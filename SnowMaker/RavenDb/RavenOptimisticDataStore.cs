7b102c9b7b44004dacfbabf6657a54fb5e31ee3f
Rob Kayman <rkayman@gmail.com>
2014-06-11 02:44:40 -0700

    add RavenDb support

---
+++ SnowMaker/RavenDb/RavenOptimisticDataStore.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using Raven.Abstractions.Exceptions;
+using Raven.Client;
+
+namespace SnowMaker.RavenDb
+{
+	public class RavenOptimisticDataStore : IOptimisticDataStore
+	{
+		private const string SeedValue = "1";
+
+		private readonly IDocumentStore store;
+
+		private readonly IDictionary<string, RavenStateDocument> openStates;
+		private readonly object statesLock;
+
+		public RavenOptimisticDataStore( IDocumentStore store )
+		{
+			this.store = store;
+			openStates = new Dictionary<string, RavenStateDocument>();
+			statesLock = new object();
+		}
+
+		public string GetData( string id )
+		{
+			lock (statesLock)
+			{
+				var state = GetStateDocument( id );
+
+				openStates[id] = state;
+	
+				return state.Value;
+			}
+		}
+
+		private RavenStateDocument GetStateDocument( string id )
+		{
+			RavenStateDocument state;
+
+			if (openStates.TryGetValue( id, out state ))
+				return state;
+
+			return RetrieveStateDocument( id ) ?? CreateStateDocument( id );
+		}
+
+		private RavenStateDocument RetrieveStateDocument( string id )
+		{
+			using (var session = store.OpenSession())
+			{
+				session.Advanced.UseOptimisticConcurrency = true;
+				var state = session.Load<RavenStateDocument>( id );
+
+				if (null != state)
+					state.Etag = session.Advanced.GetEtagFor( state );
+
+				return state;
+			}
+		}
+
+		private static RavenStateDocument CreateStateDocument( string id )
+		{
+			return new RavenStateDocument { Id = id, Value = SeedValue };
+		}
+
+		public bool TryOptimisticWrite( string id, string data )
+		{
+			using (var session = store.OpenSession())
+			{
+				try
+				{
+					var state = GetStateDocument( id );
+					state.Value = data;
+
+					session.Store( state, state.Etag, state.Id );
+					session.SaveChanges();
+				}
+				catch (ConcurrencyException)
+				{
+					openStates.Remove( id );
+					return false;
+				}
+
+				return true;
+			}
+		}
+	}
+}
---
+++ SnowMaker/RavenDb/RavenStateDocument.cs
@@ -0,0 +1,15 @@
+﻿using Raven.Abstractions.Data;
+using Raven.Imports.Newtonsoft.Json;
+
+namespace SnowMaker.RavenDb
+{
+	public class RavenStateDocument
+	{
+		public string Id { get; set; }
+
+		public string Value { get; set; }
+
+		[JsonIgnore]
+		public Etag Etag { get; set; }
+	}
+}

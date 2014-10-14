918cdddbb743d3cb966a4c1ad08c241aaf86ad03
Rob Kayman <rkayman@gmail.com>
2014-10-14 07:52:35 -0700

    Start to add async/await support for Raven
    Adding async/await support since much of this library depends on I/O.
    To be done: modify base classes, unit tests, integration tests for async
    support

--- SnowMaker/RavenOptimisticDataStore.cs
+++ SnowMaker/RavenOptimisticDataStore.cs
@@ -1,4 +1,5 @@
-﻿using Raven.Abstractions.Data;
+﻿using System.Threading.Tasks;
+using Raven.Abstractions.Data;
 using Raven.Abstractions.Exceptions;
 using Raven.Client;
 using Raven.Imports.Newtonsoft.Json;
@@ -65,6 +66,22 @@ namespace SnowMaker
 			}
 		}
 
+		private async Task<SnowMakerDocument> CreateDocumentAsync(string id)
+		{
+			using (var session = Store.OpenAsyncSession())
+			{
+				session.Advanced.UseOptimisticConcurrency = true;
+
+				var doc = new SnowMakerDocument { Id = id, Value = SeedValue };
+				await session.StoreAsync(doc, id);
+				await session.SaveChangesAsync();
+
+				doc.Etag = session.Advanced.GetEtagFor(doc);
+
+				return doc;
+			}
+		}
+
 		private SnowMakerDocument ReadDocument(string id)
 		{
 			using (var session = Store.OpenSession())
@@ -78,6 +95,19 @@ namespace SnowMaker
 			}
 		}
 
+		private async Task<SnowMakerDocument> ReadDocumentAsync(string id)
+		{
+			using (var session = Store.OpenAsyncSession())
+			{
+				var doc = await session.LoadAsync<SnowMakerDocument>(id);
+
+				if (null != doc)
+					doc.Etag = session.Advanced.GetEtagFor(doc);
+
+				return doc ?? await CreateDocumentAsync(id);
+			}
+		}
+
 		private void UpdateDocument(SnowMakerDocument doc)
 		{
 			using (var session = Store.OpenSession())
@@ -86,6 +116,19 @@ namespace SnowMaker
 
 				session.Store(doc, doc.Etag, doc.Id);
 				session.SaveChanges();
+
+				doc.Etag = session.Advanced.GetEtagFor(doc);
+			}
+		}
+
+		private async Task UpdateDocumentAsync(SnowMakerDocument doc)
+		{
+			using (var session = Store.OpenAsyncSession())
+			{
+				session.Advanced.UseOptimisticConcurrency = true;
+
+				await session.StoreAsync(doc, doc.Etag, doc.Id);
+				await session.SaveChangesAsync();
 
 				doc.Etag = session.Advanced.GetEtagFor(doc);
 			}

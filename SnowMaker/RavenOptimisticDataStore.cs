89ab12ea2f4419312b112efce02f1749213a2c18
Rob Kayman <rkayman@gmail.com>
2014-10-24 14:22:59 +0800

    Move session management as early as possible
    Session management now in GetData and WriteData public methods; session
    passed in to Create, Read and Update document methods using extension
    methods

--- SnowMaker/RavenOptimisticDataStore.cs
+++ SnowMaker/RavenOptimisticDataStore.cs
@@ -18,8 +18,6 @@ namespace SnowMaker
 
 	public class RavenOptimisticDataStore : IOptimisticDataStoreAsync
 	{
-		private const string SeedValue = "1";
-
 		private readonly IDocumentStore Store;
 
 		public RavenOptimisticDataStore(IDocumentStore store)
@@ -29,67 +27,69 @@ namespace SnowMaker
 
 		public async Task<string> GetDataAsync(string id)
 		{
-			var doc = await ReadDocumentAsync(id);
-			return doc.Value;
+			using (var session = Store.OpenAsyncSession())
+			{
+				var doc = await session.ReadDocumentAsync(id);
+				return doc.Value;				
+			}
 		}
 
 		public async Task<bool> TryOptimisticWriteAsync(string id, string data)
 		{
-			var doc = await ReadDocumentAsync(id);
-			doc.Value = data;
-
-			try
-			{
-				await UpdateDocumentAsync(doc);
-			}
-			catch (ConcurrencyException)
-			{
-				return false;
-			}
-
-			return true;
-		}
-
-		private async Task<SnowMakerDocument> CreateDocumentAsync(string id)
-		{
 			using (var session = Store.OpenAsyncSession())
 			{
-				session.Advanced.UseOptimisticConcurrency = true;
+				var doc = await session.ReadDocumentAsync(id);
+				doc.Value = data;
 
-				var doc = new SnowMakerDocument { Id = id, Value = SeedValue };
-				await session.StoreAsync(doc, id);
-				await session.SaveChangesAsync();
+				try
+				{
+					await session.UpdateDocumentAsync(doc);
+				}
+				catch (ConcurrencyException)
+				{
+					return false;
+				}
 
+				return true;
+			}
+		}
+	}
+
+	internal static class RavenSessionExtensions
+	{
+		private const string SeedValue = "1";
+
+		internal static async Task<SnowMakerDocument> CreateDocumentAsync(this IAsyncDocumentSession session, string id)
+		{
+			session.Advanced.UseOptimisticConcurrency = true;
+
+			var doc = new SnowMakerDocument { Id = id, Value = SeedValue };
+			await session.StoreAsync(doc, id);
+			await session.SaveChangesAsync();
+
+			doc.Etag = session.Advanced.GetEtagFor(doc);
+
+			return doc;
+		}
+
+		internal static async Task<SnowMakerDocument> ReadDocumentAsync(this IAsyncDocumentSession session, string id)
+		{
+			var doc = await session.LoadAsync<SnowMakerDocument>(id);
+
+			if (null != doc)
 				doc.Etag = session.Advanced.GetEtagFor(doc);
 
-				return doc;
-			}
+			return doc ?? await session.CreateDocumentAsync(id);
 		}
 
-		private async Task<SnowMakerDocument> ReadDocumentAsync(string id)
+		internal static async Task UpdateDocumentAsync(this IAsyncDocumentSession session, SnowMakerDocument doc)
 		{
-			using (var session = Store.OpenAsyncSession())
-			{
-				var doc = await session.LoadAsync<SnowMakerDocument>(id);
+			session.Advanced.UseOptimisticConcurrency = true;
 
-				if (null != doc)
-					doc.Etag = session.Advanced.GetEtagFor(doc);
+			await session.StoreAsync(doc, doc.Etag, doc.Id);
+			await session.SaveChangesAsync();
 
-				return doc ?? await CreateDocumentAsync(id);
-			}
-		}
-
-		private async Task UpdateDocumentAsync(SnowMakerDocument doc)
-		{
-			using (var session = Store.OpenAsyncSession())
-			{
-				session.Advanced.UseOptimisticConcurrency = true;
-
-				await session.StoreAsync(doc, doc.Etag, doc.Id);
-				await session.SaveChangesAsync();
-
-				doc.Etag = session.Advanced.GetEtagFor(doc);
-			}
+			doc.Etag = session.Advanced.GetEtagFor(doc);
 		}
 	}
 }

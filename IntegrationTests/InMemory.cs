a925f492f4d7412f93bf96060ae6688bb0d3109d
Rob Kayman <rkayman@gmail.com>
2015-02-22 15:44:55 -0800

    Add InMemoryOptimisticDataStore
    Used for debugging purposes; leverages ConcurrentDictionary

---
+++ IntegrationTests/InMemory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Concurrent;
+using NUnit.Framework;
+using SnowMaker;
+
+namespace IntegrationTests.cs
+{
+	[TestFixture]
+	public class InMemory : Scenarios<InMemory.TestScope>
+	{
+		protected override TestScope BuildTestScope()
+		{
+			return new TestScope();
+		}
+
+		protected override IOptimisticDataStore BuildStore(TestScope scope)
+		{
+			return new InMemoryOptimisticDataStore(scope.OptimisticDictionary);
+		}
+
+		public sealed class TestScope : ITestScope
+		{
+			public TestScope()
+			{
+				var ticks = DateTime.UtcNow.Ticks;
+				IdScopeName = string.Format("snowmakertest{0}", ticks);
+				OptimisticDictionary = new ConcurrentDictionary<string, string>();
+			}
+
+			public string IdScopeName { get; private set; }
+
+			public ConcurrentDictionary<string, string> OptimisticDictionary { get; private set; } 
+
+			public string ReadCurrentPersistedValue()
+			{
+				string value;
+				var success = OptimisticDictionary.TryGetValue(IdScopeName, out value);
+				return success ? value : string.Empty;
+			}
+
+			public void Dispose()
+			{
+				OptimisticDictionary.Clear();
+			}
+		}
+	}
+}
---
+++ SnowMaker/InMemoryOptimisticDataStore.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+
+namespace SnowMaker
+{
+	public class InMemoryOptimisticDataStore : IOptimisticDataStore
+	{
+		private const string SeedValue = "1";
+
+		private readonly ConcurrentDictionary<string, string> OptimisticDictionary;
+
+		public InMemoryOptimisticDataStore()
+		{
+			OptimisticDictionary = new ConcurrentDictionary<string, string>();
+		}
+
+		public InMemoryOptimisticDataStore(ConcurrentDictionary<string, string> optimisticDictionary)
+		{
+			OptimisticDictionary = optimisticDictionary;
+		}
+
+		public string GetData(string blockName)
+		{
+			return OptimisticDictionary.GetOrAdd(blockName, SeedValue);
+		}
+
+		public bool TryOptimisticWrite(string blockName, string data)
+		{
+			return OptimisticDictionary.AddOrUpdate(blockName, data, (key, value) => data).Equals(data);
+		}
+	}
+}
